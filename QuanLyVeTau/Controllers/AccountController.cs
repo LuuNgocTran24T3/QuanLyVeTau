6fcbcc81ec79a5a2c617d607528075e57998f498
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-29 17:33:39 +0700

    create login view

---
+++ QuanLyVeTau/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyVeTau.Models;
+
+namespace QuanLyVeTau.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly VeTauDbCaiTienContext _context;
+
+        // constructor
+        public AccountController(VeTauDbCaiTienContext context)
+        {
+            _context = context;
+        }
+
+        // GET LOGIN
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST LOGIN
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            var user = _context.KhachHangs
+                .FirstOrDefault(x =>
+                    x.Email == email &&
+                    x.MatKhauHash == password);
+
+            if (user != null)
+            {
+                HttpContext.Session.SetString(
+                    "user",
+                    user.Email
+                );
+
+                return RedirectToAction(
+                    "Index",
+                    "Home"
+                );
+            }
+
+            ViewBag.Error = "Sai tài khoản hoặc mật khẩu";
+
+            return View();
+        }
+
+        // LOGOUT
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
