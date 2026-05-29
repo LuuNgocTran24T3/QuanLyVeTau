359d090bc36d3305a6160903a5916b56a98e157b
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-30 01:06:19 +0700

    add extensions

---
+++ QuanLyVeTau/Extensions/StringExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace QuanLyVeTau.Extensions
+{ 
+    public static class StringExtension 
+    { 
+        public static string ToUpperFirst(this string text) 
+        { 
+            if (string.IsNullOrEmpty(text)) 
+                return text; 
+            return char.ToUpper(text[0]) + text.Substring(1); 
+        } 
+    } 
+}
