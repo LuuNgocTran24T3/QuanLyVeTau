fe92faf13c8f1e0b9820f6d3679b497815d88f39
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-30 01:15:56 +0700

    add viewmodel

---
+++ QuanLyVeTau/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace QuanLyVeTau.ViewModels 
+{ 
+    public class LoginViewModel 
+    { 
+        [Required] 
+        public string Email { get; set; } 
+        [Required] 
+        public string Password { get; set; } 
+    } 
+}
