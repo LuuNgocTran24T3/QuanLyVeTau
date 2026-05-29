c3b63455e066f90eb6498060a302014cbfd817a2
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-30 01:09:40 +0700

    add custom validation

---
+++ QuanLyVeTau/Validations/EmailValidation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace VeTauMVC.Validations 
+{ 
+    public class EmailValidation : ValidationAttribute 
+    { 
+        public override bool IsValid(object value) 
+        { 
+            if (value == null) 
+                return false; 
+            return value.ToString().Contains("@"); 
+        } 
+    } 
+}
