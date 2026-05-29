f760e82a57f2293ef68d536a50868ab218bac8dc
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-30 00:59:01 +0700

    add hardcode

---
+++ QuanLyVeTau/Hardcodes/SystemRole.cs
@@ -0,0 +1,8 @@
+﻿namespace QuanLyVeTau.Hardcodes
+{ 
+    public static class SystemRole 
+    { 
+        public const string Staff = "Staff"; 
+        public const string Customer = "Customer"; 
+    } 
+}
