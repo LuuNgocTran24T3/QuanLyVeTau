a4d37a638a5865152dd98137ba6dbd6b46d1d697
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-30 01:19:06 +0700

    add repositories

---
+++ QuanLyVeTau/Repositories/TrainRepository.cs
@@ -0,0 +1,16 @@
+﻿using QuanLyVeTau.Models;
+namespace QuanLyVeTau.Repositories 
+{ 
+    public class TrainRepository 
+    { 
+        private readonly VeTauDbCaiTienContext _context; 
+        public TrainRepository(VeTauDbCaiTienContext context) 
+        { 
+            _context = context; 
+        } 
+        public List<Tau> GetAll() 
+        { 
+            return _context.Taus.ToList(); 
+        } 
+    } 
+}
