295f68666d26dc8b9f4cf31f86c7a9ca40410700
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-30 01:03:48 +0700

    add helper classes

---
+++ QuanLyVeTau/Helper/MoneyHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace QuanLyVeTau.Helper
+{ 
+    public static class MoneyHelper 
+    { 
+        public static string FormatMoney(decimal money) 
+        { 
+            return string.Format("{0:N0} VNĐ", money); 
+        } 
+    } 
+}
