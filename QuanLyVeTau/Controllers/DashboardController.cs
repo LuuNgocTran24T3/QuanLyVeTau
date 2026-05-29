b9c2db7e1fbcd60b9242e89bcef048af21fe76e8
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-30 00:10:51 +0700

    add admin dashboard

---
+++ QuanLyVeTau/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyVeTau.Models;
+
+public class DashboardController : Controller
+{
+    private readonly VeTauDbCaiTienContext _context;
+
+    public DashboardController(VeTauDbCaiTienContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        if (HttpContext.Session.GetString("role") != "Staff")
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        ViewBag.TotalTrain = _context.Taus.Count();
+        ViewBag.TotalTicket = _context.Ves.Count();
+        ViewBag.TotalCustomer = _context.KhachHangs.Count();
+
+        return View();
+    }
+}
