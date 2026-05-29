834fec03235d20597cd67b67838012da9e7cf757
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-05-30 01:01:04 +0700

    add constant

---
+++ QuanLyVeTau/Constants/MessageConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace QuanLyVeTau.Constants
+{ 
+    public static class MessageConstant 
+    { 
+        public const string LoginFail = "Sai tài khoản hoặc mật khẩu"; 
+        public const string CreateSuccess = "Thêm thành công"; 
+        public const string UpdateSuccess = "Cập nhật thành công"; 
+        public const string DeleteSuccess = "Xóa thành công"; 
+    } 
+}
